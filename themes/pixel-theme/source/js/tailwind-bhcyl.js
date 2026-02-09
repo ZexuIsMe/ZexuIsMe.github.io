8f0c3367944db760dd160fa7aaead85a1211838e
he_ze <he_zexu@126.com>
2026-02-09 16:59:51 +0800

    add 自定义字体和样式更新

--- themes/pixel-theme/source/js/tailwind-bhcyl.js
+++ themes/pixel-theme/source/js/tailwind-bhcyl.js
@@ -14,7 +14,10 @@ tailwind.config = {
             },
             fontFamily: {
                 sans: ['Inter', 'sans-serif'],
-                serif: ['Noto Serif SC', 'serif']
+                serif: ['Noto Serif SC', 'serif'],
+                gufeng: ['ZiKuJiangHuGuFengTi', 'cursive'],
+                xingqiu: ['ZiKuXingQiuFeiYangTi', 'cursive'],
+                pingfang: ['PingFangLaiJiangHuFeiYangTi', 'cursive']
             },
             backgroundImage: {
                 'ancient-pattern': "url('https://p11-flow-imagex-sign.byteimg.com/tos-cn-i-a9rns2rl98/rc/pc/super_tool/27c19c45584943e3b04b6311583ac9a6~tplv-a9rns2rl98-image.image?lk3s=8e244e95&rcl=202602052337540D3F64686579370B2CC5&rrcfp=f06b921b&x-expires=1772897936&x-signature=A9o168eafNI0ZJrcS8%2BDuYxtvjI%3D')"
